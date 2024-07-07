8775edddb984d5abc05af1a9ba3287409e960e4a
Volodrum <148470566+Volodrum@users.noreply.github.com>
2024-07-07 12:38:10 +0300

    create calculator code

---
+++ GitAspDemo/GitAspDemo/Controllers/CalculatorController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitAspDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("calc/")]
+        public ActionResult<string> Calc(decimal x, decimal y, string op)
+        {
+            decimal res;
+            switch (op)
+            {
+                case "+":
+                    res = x + y;
+                    break;
+                case "-":
+                    res = x - y;
+                    break;
+                case "*":
+                    res = x * y;
+                    break;
+                case "/":
+                    if (y == 0)
+                        return "Result: Infinity";
+                    res = x / y;
+                    break;
+                default:
+                    return "Invalid operation!";
+            }
+            return $"Result: {res}";
+        }
+    }
+}
