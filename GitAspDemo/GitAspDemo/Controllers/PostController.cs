587dc7697ff6c8b021cac8aa2a1bc3d1669dfa5e
Volodrum <148470566+Volodrum@users.noreply.github.com>
2024-07-07 12:53:33 +0300

    add post controller

---
+++ GitAspDemo/GitAspDemo/Controllers/PostController.cs
@@ -0,0 +1,33 @@
+﻿using DocumentFormat.OpenXml.Drawing.Diagrams;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitAspDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        // POST api/postexample
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] string value)
+        {
+
+            return Ok($"Отримано значення: {value}");
+        }
+
+        // POST api/postexample/data
+        [HttpPost("data")]
+        public ActionResult<object> PostData([FromBody] DataModel data)
+        {
+
+            return Ok(new { Message = "Дані успішно отримано", Data = data });
+        }
+    }
+
+    public class DataModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
